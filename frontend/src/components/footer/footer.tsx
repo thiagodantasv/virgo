b6da19a178496b0b355660ff34b23e36aae0a6f5
thiagodantasv <thiagodantasv@gmail.com>
2020-10-29 23:32:39 -0300

    Creating the footer and the footer css

---
+++ frontend/src/components/footer/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.css';
+function Footer(){
+    return(
+        <>
+            <footer>
+                <span>Desenvolvido por Thiago Dantas Vasco</span>
+                <i className="fab fa-github"></i>
+            </footer>
+        </>
+    );
+}
+export default Footer;
