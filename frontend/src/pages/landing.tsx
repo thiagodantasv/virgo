686d8fde33df7936aa081649b58084adbd9b769b
thiagodantasv <thiagodantasv@gmail.com>
2020-11-04 04:40:58 -0300

    landing page basic skeleton

--- frontend/src/pages/landing.tsx
+++ frontend/src/pages/landing.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './../style/landing.css';
 
 function Landing(){
     return(
         <>
-            <h1>Welcome to my website</h1>
+            <header>
+                <h1>Você no controle das suas carteiras de investimentos</h1>
+            </header>
+            <div className = "landing-page-button">
+                <button onClick = { ()=> {console.log('i am a sign up button')} }>Cadastre-se</button>
+            </div>
         </>
     );
 }
